5326dec01719f0dc86e327d10dc58b774ea4e172
ilyua <old.flanigan@gmail.com>
2017-12-17 20:51:00 +0300

    2.3 - bash.txt информация о zeros и nozeros

---
+++ bash_txt.sh
@@ -0,0 +1,15 @@
+Z=/tmp/zeros
+NOZ=/tmp/nozeros
+
+grep 000000 ~/bash.txt > $Z
+grep -v 000000 ~/bash.txt > $NOZ
+echo -e "\n\n Head:"
+echo "zeros"
+head -n 10 $Z
+echo "nozeros"
+head -n 10 $NOZ
+echo -e "\n\n Tail:"
+echo "zeros"
+tail -n 10 $Z
+echo "nozeros"
+tail -n 10 $NOZ
